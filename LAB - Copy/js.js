cf9f4b0bb77ed9a8da3de33856878749800efc91
JamsAGB <webdevagb@gmail.com>
2023-04-23 13:54:29 +0800

    updated

--- LAB - Copy/js.js
+++ LAB - Copy/js.js
@@ -1,34 +1,77 @@
-// SAMPLE NEST FUNCTION
-
-// function test() {
-//     var ran = document.getElementById("test");
-//     if (ran.innerHTML == "Click") {
-//         result();
-//     }
-// }
-
-// function result() {
-//     console.log("Success!");
-// }
-
+// VARIABLES
 const beverageContainer = document.getElementById('beverages-container');
 const breadContainer = document.getElementById('bread-container');
 const cannedContainer = document.getElementById('canned-container');
-const card = document.getElementsByClassName('card')
-
+const cards = document.getElementsByClassName('card')
 const billButton = document.getElementById('bill-confirm')
 const billAmount = document.getElementById('amount')
+const purchaseButton = document.getElementById('purchase-button')
+
+
+const activeItem = document.getElementsByClassName('active-item')
+const price = document.getElementsByClassName('price')
+const total = document.getElementById('total')
+let totalVar = 0;
+total.disabled = true
+
+// LINK VARIABLES
 
 const beveragelink = document.getElementById('beverages-link');
 const breadlink = document.getElementById('bread-link');
 const cannedlink = document.getElementById('canned-link');
 
 
+// BILL INPUT CODE
+
+billButton.addEventListener('click', function() {
+    if (billAmount.value <= 60) {
+        beveragelink.classList.remove('disabled-link')
+        beverageContainer.classList.remove('disabled-container')
+        cannedlink.classList.remove('disabled-link')
+        cannedContainer.classList.remove('disabled-container')
+    } else {
+        breadlink.classList.remove('disabled-link')
+        beveragelink.classList.remove('disabled-link')
+        cannedlink.classList.remove('disabled-link')
+        beverageContainer.classList.remove('disabled-container')
+        cannedContainer.classList.remove('disabled-container')
+        breadContainer.classList.remove('disabled-container')
+    } 
+})
 
 
+// TOTAL VALUE CODE
+
+for(i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', function() {
+        this.classList.toggle('item-container')
 
 
+        // ChatGPT 
+      const value = parseFloat(this.getAttribute('data-value'));
+      totalVar += this.classList.contains('item-container') ? value : -value;
+      total.value = totalVar.toFixed(2);
+    })
+}
 
+
+// TOTAL CHECK CODE
+
+purchaseButton.addEventListener('click', function() {
+    let billAmountValue = parseFloat(billAmount.value);
+    let totalValue = parseFloat(total.value);
+    let change = billAmountValue - totalValue;
+
+    if (billAmountValue == totalValue) {
+        alert("Thank you for purchasing!")
+        location.reload()
+    } else if (billAmountValue > totalValue) {
+        alert("Thank you for purchasing! \n Your Change: " + change)
+        location.reload()
+    } else if (billAmountValue < totalValue) {
+        alert("Insufficient Funds!")
+    }
+})
 
 
 
@@ -51,8 +94,6 @@ cannedlink.addEventListener('click', function() {
 });
 
 
-for(i = 0; i < card.length; i++) {
-    card[i].addEventListener('click', function() {
-        this.classList.toggle('item-container')
-    })
-}
+
+
+
